462f53e0788b2e4e0ca872825c2412e36374daa7
dmalves <tcleval@gmail.com>
2023-10-29 20:10:42 -0300

    changed root password

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'c5f169a877b5090da0a0b988fec8b67821b221c1'
+AESD_ASSIGNMENTS_VERSION = '1e788e66736175e26be4f39a9636ead9a58ff663'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
