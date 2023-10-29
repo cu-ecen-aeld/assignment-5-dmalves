c724c67338c76c3f328e73af372c36c85f2cf877
dmalves <tcleval@gmail.com>
2023-10-29 19:30:13 -0300

    finder.sh finder-test.sh and writer working on qemu

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '566a76f1b459fa08a0c798b904b3a3f84d71421a'
+AESD_ASSIGNMENTS_VERSION = 'c5f169a877b5090da0a0b988fec8b67821b221c1'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
